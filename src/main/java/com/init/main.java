da91b8c060d47f3c82716db5d6115547ee922a75
davin <deyong1994@163.com>
2019-10-15 15:49:03 +0800

    init commit

---
+++ src/main/java/com/init/main.java
@@ -0,0 +1,12 @@
+package com.init;
+
+/**
+ * @author davin
+ * @date 2019-10-15 15:23
+ */
+public class main {
+
+    public static void main(String[] args) {
+        System.out.println("hello world");
+    }
+}
